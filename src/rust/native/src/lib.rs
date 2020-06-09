d1f91fa1084c3e9112f7b29d7138cb2eeb5c33a3
Vale <valerauko@gmail.com>
2020-06-10 01:38:02 +0900

    Add rust backend

---
+++ src/rust/native/src/lib.rs
@@ -0,0 +1,22 @@
+use neon::prelude::*;
+use neon::register_module;
+
+fn hello_world(mut cx: FunctionContext) -> JsResult<JsString> {
+    let path = "/home/valerauko/Code/Kitsune/kitsune".to_string();
+    let message = match git2::Repository::open(&path) {
+        Ok(repo) => match repo.head() {
+            Ok(head) => match head.peel_to_commit() {
+                Ok(commit) => match commit.summary() {
+                    Some(msg) => msg.to_string(),
+                    None => commit.id().to_string(),
+                },
+                Err(_) => "HEAD is not a commit".to_string(),
+            },
+            Err(_) => "No HEAD".to_string(),
+        },
+        Err(_) => "Couldn't open repo".to_string(),
+    };
+    Ok(cx.string(format!("The last commit {}.", message)))
+}
+
+register_module!(mut m, { m.export_function("lastCommit", hello_world) });
