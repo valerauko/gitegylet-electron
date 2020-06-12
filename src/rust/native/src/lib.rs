41fbb90de8cc6a7989a934d2207f8f4c2fbc9995
Vale <valerauko@gmail.com>
2020-06-12 20:26:35 +0900

    Get rid of last_commit
    It was only there to test functionality

--- src/rust/native/src/lib.rs
+++ src/rust/native/src/lib.rs
@@ -29,26 +29,7 @@ fn local_branches(mut cx: FunctionContext) -> JsResult<JsArray> {
     Ok(js_array)
 }
 
-fn last_commit(mut cx: FunctionContext) -> JsResult<JsString> {
-    let path = "/home/valerauko/Code/Kitsune/kitsune".to_string();
-    let message = match git2::Repository::open(&path) {
-        Ok(repo) => match repo.head() {
-            Ok(head) => match head.peel_to_commit() {
-                Ok(commit) => match commit.summary() {
-                    Some(msg) => msg.to_string(),
-                    None => commit.id().to_string(),
-                },
-                Err(_) => "HEAD is not a commit".to_string(),
-            },
-            Err(_) => "No HEAD".to_string(),
-        },
-        Err(_) => "Couldn't open repo".to_string(),
-    };
-    Ok(cx.string(format!("The last commit {}.", message)))
-}
-
 register_module!(mut m, {
-    m.export_function("lastCommit", last_commit)?;
     m.export_function("localBranches", local_branches)?;
     Ok(())
 });
