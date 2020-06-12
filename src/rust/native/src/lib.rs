c1d31839aeea954174b763d46b0c4faa987efc5b
Vale <valerauko@gmail.com>
2020-06-12 20:27:22 +0900

    Add Commit struct

--- src/rust/native/src/lib.rs
+++ src/rust/native/src/lib.rs
@@ -1,6 +1,57 @@
 use neon::prelude::*;
 use neon::register_module;
 
+use std::cmp::Ordering;
+
+#[derive(Clone)]
+struct Commit {
+    id: git2::Oid,
+    time: git2::Time,
+    summary: String,
+    message: String,
+    author: git2::Signature<'static>,
+    selected: bool,
+}
+
+impl Commit {
+    fn from_git2(commit: git2::Commit) -> Self {
+        Self {
+            id: commit.id(),
+            time: commit.time(),
+            summary: match commit.summary() {
+                Some(summary) => summary.to_string(),
+                None => commit.id().to_string(),
+            },
+            message: match commit.message() {
+                Some(msg) => msg.to_string(),
+                None => commit.id().to_string(),
+            },
+            author: commit.author().to_owned(),
+            selected: false,
+        }
+    }
+}
+
+impl Ord for Commit {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.time.cmp(&other.time)
+    }
+}
+
+impl PartialOrd for Commit {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Commit {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+
+impl Eq for Commit {}
+
 fn local_branches(mut cx: FunctionContext) -> JsResult<JsArray> {
     let path = "/home/valerauko/Code/Kitsune/kitsune".to_string();
     let branches = match git2::Repository::open(&path) {
@@ -31,5 +82,6 @@ fn local_branches(mut cx: FunctionContext) -> JsResult<JsArray> {
 
 register_module!(mut m, {
     m.export_function("localBranches", local_branches)?;
+    m.export_function("commits", commits)?;
     Ok(())
 });
