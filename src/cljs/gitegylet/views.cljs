03e3d026fa3deff80e6b839162fe758a1c4f7b49
Vale <valerauko@gmail.com>
2020-06-13 00:43:23 +0900

    Add branch and commit list

--- src/cljs/gitegylet/views.cljs
+++ src/cljs/gitegylet/views.cljs
@@ -1,33 +1,59 @@
 (ns gitegylet.views
   (:require [reagent.core  :as reagent]
-            [re-frame.core :as rf :refer [subscribe dispatch]]
-            [clojure.string :as str]))
+            [re-frame.core :as rf]
+            [clojure.string :refer [split join]]
+            ["react-checkbox-tree" :as CheckboxTree]
+            [gitegylet.events :as events]
+            [gitegylet.subs :as subs]))
 
-(def hello (js/require "git"))
+(defn nested-branch-reducer
+  [aggr item]
+  (let [children (rest item)]
+    (if (empty? children)
+      aggr
+      (update aggr :children conj children))))
 
-;; -- Domino 5 - View Functions ----------------------------------------------
-
-(defn clock
-  []
-  [:div.example-clock
-   {:style {:color @(rf/subscribe [:time-color])}}
-   (-> @(rf/subscribe [:time])
-       .toTimeString
-       (str/split " ")
-       first)])
-
-(defn color-input
-  []
-  [:div.color-input
-   "Time color: "
-   [:input {:type "text"
-            :value @(rf/subscribe [:time-color])
-            :on-change #(rf/dispatch [:time-color-change (-> % .-target .-value)])}]])  ;; <---
+(defn group-branches
+  ([items] (group-branches items []))
+  ([items parents]
+   (->> items
+        (partition-by first)
+        (map
+         (fn [folder]
+           (let [label (-> folder first first)
+                 new-parents (conj parents label)
+                 layer (reduce
+                         nested-branch-reducer
+                         {:label label
+                           :value (join "/" new-parents)}
+                         folder)]
+             (if (empty? (:children layer))
+               layer
+               (update
+                 layer
+                 :children
+                 #(group-branches % new-parents)))))))))
 
 (defn ui
   []
-  [:div
-   [:h1 "Hello world, it is now"]
-   [:p (.lastCommit hello)]
-   [clock]
-   [color-input]])
+  (let [nodes (->> @(rf/subscribe [::subs/branches])
+                   (map #(split % #"/"))
+                   (group-branches))
+        commits @(rf/subscribe [::subs/commits])
+        checked @(rf/subscribe [::subs/branches-checked])
+        expanded @(rf/subscribe [::subs/branches-expanded])
+        on-check #(rf/dispatch [::events/branch-check %])
+        on-expand #(rf/dispatch [::events/branch-expand %])]
+    [:div {:id "flex"}
+     [:div {:class "branches"}
+      [:> CheckboxTree
+       {:nodes nodes
+        :checked checked
+        :expanded expanded
+        :only-leaf-checkboxes true
+        :on-check on-check
+        :on-expand on-expand}]]
+     [:div {:class "commits"}
+      (->> commits
+           (map (fn [msg] [:li msg]))
+           (into [:ol]))]]))
