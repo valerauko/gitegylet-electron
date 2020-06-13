6e9889d728f1300000ee28b2a9f861c0b9a753d5
Vale <valerauko@gmail.com>
2020-06-13 21:00:00 +0900

    Persist to local storage

--- src/cljs/gitegylet/db.cljs
+++ src/cljs/gitegylet/db.cljs
@@ -1,6 +1,19 @@
 (ns gitegylet.db
-  (:require [cljs.reader]
-            [cljs.spec.alpha :as s]
+  (:require [cljs.spec.alpha :as s]
+            [cljs.reader :refer [read-string]]
             [re-frame.core :as rf]))
 
 ;; TODO: add schema specs
+
+(defn ->local-storage
+  "Persists DB to localStorage"
+  [db]
+  (.setItem js/localStorage "gitegylet" (str db)))
+
+(rf/reg-cofx
+ ::local-storage
+ (fn [state _]
+   (assoc state :local-storage
+     (some->> "gitegylet"
+              (.getItem js/localStorage)
+              (read-string)))))
--- src/cljs/gitegylet/events.cljs
+++ src/cljs/gitegylet/events.cljs
@@ -2,20 +2,28 @@
   (:require
     [re-frame.core :as rf]
     [cljs.spec.alpha :as s]
-    [gitegylet.git :refer [git]]))
+    [gitegylet.git :refer [git]]
+    [gitegylet.db :as db]))
+
+(def ->local-storage
+  "Interceptor to persist database"
+  (rf/after db/->local-storage))
 
 (rf/reg-event-fx
   ::initialize-db
-  (fn [_ _]
-    {:db {:repo "."}}))
+  [(rf/inject-cofx ::db/local-storage)]
+  (fn [{:keys [db local-storage]}]
+    {:db (merge {:repo "."} local-storage)}))
 
 (rf/reg-event-db
  ::branch-select
+ [->local-storage]
  (fn [db [_ item]]
    (assoc db :branches-selected item)))
 
 (rf/reg-event-db
  ::folder-expand
+ [->local-storage]
  (fn [db [_ item]]
    (assoc db :folders-expanded item)))
 
@@ -29,6 +37,7 @@
 
 (rf/reg-event-db
  ::open-repo
+ [->local-storage]
  (fn [db [_ folder]]
    ; if the dialog was cancelled folder is going to be empty
    (if folder
