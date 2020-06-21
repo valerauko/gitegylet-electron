7b88e3bbf7269a2cf82cd0856542a3fa34f5cb12
Vale <valerauko@gmail.com>
2020-06-21 15:47:52 +0900

    Implement basic fetch

--- src/rust/native/src/branch.rs
+++ src/rust/native/src/branch.rs
@@ -1,4 +1,5 @@
 use serde::ser::{Serialize, Serializer, SerializeStruct};
+use std::collections::{HashSet, HashMap};
 
 #[derive(Clone)]
 pub struct Branch {
@@ -67,6 +68,61 @@ impl Branch {
             },
             Err(_) => aggr,
         })
+    }
+
+    pub fn fetch(repo: git2::Repository, branch_names: Vec<String>) -> Vec<Self> {
+        let mut remotes = HashSet::new();
+        let mut branch_by_remote: HashMap<String, Vec<String>> = HashMap::new();
+
+        branch_names
+            .iter()
+            .for_each(|name| match repo.find_branch(name, git2::BranchType::Local) {
+                Ok(branch) => match branch.upstream() {
+                    Ok(upstream) => match upstream.name() {
+                        Ok(Some(upstream_name)) => {
+                            let pieces: Vec<&str> = upstream_name.splitn(2, "/").collect();
+                            let remote_name = pieces[0].to_string();
+                            let branch_name = pieces[1].to_string();
+                            remotes.insert(remote_name.clone());
+                            (*branch_by_remote.entry(remote_name).or_insert(vec![]))
+                                .push(branch_name);
+                        },
+                        Err(e) => println!("{}", e),
+                        _ => {},
+                    },
+                    Err(e) => println!("{}", e),
+                },
+                Err(e) => println!("{}", e),
+            });
+
+        remotes
+            .iter()
+            .for_each(|remote_name| match repo.find_remote(remote_name) {
+                Ok(mut remote) => match remote.fetch(&branch_by_remote[remote_name], None, None) {
+                    Err(e) => println!("{}", e),
+                    _ => {},
+                },
+                Err(e) => println!("{}", e),
+            });
+
+        branch_names
+            .iter()
+            .fold(vec![], |mut aggr, branch_name| match repo.find_branch(branch_name, git2::BranchType::Local) {
+                Ok(branch) => match Branch::from_git2(&repo, branch) {
+                    Ok(branch) => {
+                        aggr.push(branch);
+                        aggr
+                    },
+                    Err(e) => {
+                        println!("{}", e);
+                        aggr
+                    },
+                },
+                Err(e) => {
+                    println!("{}", e);
+                    aggr
+                },
+            })
     }
 }
 
