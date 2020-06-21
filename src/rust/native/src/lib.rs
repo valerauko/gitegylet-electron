38e6c562e551fc67de2dbc5cb6c4c4dd66a35421
Vale <valerauko@gmail.com>
2020-06-21 15:48:04 +0900

    Export fetch

--- src/rust/native/src/lib.rs
+++ src/rust/native/src/lib.rs
@@ -47,6 +47,33 @@ fn commits(mut cx: FunctionContext) -> JsResult<JsArray> {
     Ok(js_commits)
 }
 
+fn fetch(mut cx: FunctionContext) -> JsResult<JsArray> {
+    let arr_handle: Handle<JsArray> = cx.argument(1)?;
+    let js_array: Vec<Handle<JsValue>> = arr_handle.to_vec(&mut cx)?;
+    let branch_names: Vec<String> = js_array
+        .iter()
+        .map(|js_value| {
+            js_value
+                .downcast::<JsString>()
+                .unwrap()
+                .value()
+        })
+        .collect();
+    let js_path: Handle<JsString> = cx.argument(0)?;
+    let repo_path: String = js_path.downcast::<JsString>().unwrap().value();
+    let repo = git2::Repository::open(&repo_path).unwrap();
+
+    let branches = Branch::fetch(repo, branch_names);
+
+    let js_array = JsArray::new(&mut cx, branches.len() as u32);
+    for (i, branch) in branches.iter().enumerate() {
+        let js_branch = neon_serde::to_value(&mut cx, &branch)?;
+        js_array.set(&mut cx, i as u32, js_branch)?;
+    }
+
+    Ok(js_array)
+}
+
 fn local_branches(mut cx: FunctionContext) -> JsResult<JsArray> {
     let js_path: Handle<JsString> = cx.argument(0)?;
     let repo_path: String = js_path.downcast::<JsString>().unwrap().value();
@@ -65,6 +92,7 @@ fn local_branches(mut cx: FunctionContext) -> JsResult<JsArray> {
 
 register_module!(mut m, {
     m.export_function("localBranches", local_branches)?;
+    m.export_function("fetch", fetch)?;
     m.export_function("commits", commits)?;
     m.export_function("head", head)?;
     Ok(())
