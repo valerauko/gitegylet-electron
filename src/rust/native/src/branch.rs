a146304e20fb37e9b6b321c38b11bb70a30258d0
Vale <valerauko@gmail.com>
2020-06-21 15:47:29 +0900

    from_git2 for easier instances

--- src/rust/native/src/branch.rs
+++ src/rust/native/src/branch.rs
@@ -9,6 +9,42 @@ pub struct Branch {
 }
 
 impl Branch {
+    pub fn from_git2(repo: &git2::Repository, branch: git2::Branch) -> Result<Self, git2::Error> {
+        let name = match branch.name() {
+            Ok(Some(name)) => name.to_string(),
+            Ok(None) => return Err(git2::Error::from_str("Invalid branch name")),
+            Err(e) => return Err(e),
+        };
+        let commit_id = branch.get().peel_to_commit()?.id();
+
+        let ahead_behind = match branch.upstream() {
+            Ok(upstream) => match upstream.get().peel_to_commit() {
+                Ok(commit) => match repo.graph_ahead_behind(commit_id, commit.id()) {
+                    Ok(ab) => Some(ab),
+                    Err(e) => {
+                        println!("{}", e);
+                        None
+                    }
+                },
+                Err(e) => {
+                    println!("{}", e);
+                    None
+                },
+            },
+            Err(e) => {
+                println!("{}", e);
+                None
+            },
+        };
+
+        Ok(Self {
+            commit_id: commit_id.to_string(),
+            name,
+            is_head: branch.is_head(),
+            ahead_behind,
+        })
+    }
+
     pub fn locals(repo: git2::Repository) -> Vec<Self> {
         let branches = repo.branches(Some(git2::BranchType::Local)).unwrap();
         branches.fold(vec![], |mut aggr, branch| match branch {
